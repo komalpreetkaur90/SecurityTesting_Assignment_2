ac4b2f8f6f8b9a536bc6f9ba54085994febe6c1e
Komalpreet kaur <kkaur209@academic.rrc.ca>
2025-11-03 14:33:31 -0600

    updates the name for java file

--- assignment1_code_sample.java
+++ assignment1_code_sample.java
@@ -7,7 +7,7 @@ import java.net.URL;
 import java.sql.*;
 import java.util.Scanner;
 
-public class VulnerableApp {
+class VulnerableApp {
 
     private static final String DB_URL = "jdbc:mysql://mydatabase.com/mydb";
     private static final String DB_USER = "admin";
